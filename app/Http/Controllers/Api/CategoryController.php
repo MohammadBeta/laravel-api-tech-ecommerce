638ce3db548a339d2ced93facc6819e3f6ee0ed0
Mohammad Beta <swiftbooksjob@hotmail.com>
2024-06-30 14:17:42 +0300

    add categories api

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    use ApiResponseTrait;
+    public function index()
+    {
+        $cateogries = (Category::all());
+        if ($cateogries) {
+            return $this->apiResponse(CategoryResource::collection($cateogries), 200, 'OK');
+
+        }
+        return $this->apiResponse(null, 404, 'Empty Categories');
+
+    }
+}
---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponseTrait
+{
+    function apiResponse($data, $status, $msg){
+$responseBody = [
+    'data'=>$data,
+    'status'=>$status,
+    'message'=>$msg
+];
+return $responseBody;
+    }
+}
